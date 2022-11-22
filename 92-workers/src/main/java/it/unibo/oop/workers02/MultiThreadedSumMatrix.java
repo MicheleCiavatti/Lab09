ab754e0353b2a8b9d9d9f88e7a3034be014063d0
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2022-11-22 17:29:15 +0100

    Solution withour List

--- 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -2,32 +2,38 @@ package it.unibo.oop.workers02;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class MultiThreadedSumMatrix implements SumMatrix {
 
     private final int nThreads; 
 
     public MultiThreadedSumMatrix(final int nThreads) {
+        if (nThreads < 1) {
+            throw new IllegalArgumentException();
+        }
         this.nThreads = nThreads;
     }
 
     private static class Worker extends Thread {
-        private final List<Double> list;
+        private final double[][] matrix;
         private final int startPos;
         private final int nElems;
         private double res;
 
-        Worker(final List<Double> list, final int startPos, final int nElems) {
+        Worker(final double[][] matrix, final int startPos, final int nElems) {
             super();
-            this.list = list;
+            this.matrix = matrix;
             this.startPos = startPos;
             this.nElems = nElems;
         }
 
         public void run() {
-            System.out.println("Working from positon " + this.startPos + " to position " + (this.startPos + nElems - 1));
-            for (int i = this.startPos; i < this.list.size() && i < this.startPos + this.nElems; i++) {
-                this.res += this.list.get(i);
+            System.out.println("Working on " + (this.startPos + this.nElems) + " numbers from each row");
+            for (int i = this.startPos; i < this.matrix.length && i < this.startPos + this.nElems; i++) {
+                for (final var number: this.matrix[i]) {
+                    this.res += number;
+                }
             }
         }
 
@@ -38,31 +44,24 @@ public final class MultiThreadedSumMatrix implements SumMatrix {
     }
     @Override
     public double sum(double[][] matrix) {
-        //The matrix is converted in a list
-        final List<Double> list = new ArrayList<Double>(matrix.length);
-        for (var row: matrix) { 
-            for (var number: row) {
-                list.add(number);
-            }
-        }
-        final int size = list.size() % this.nThreads + list.size() / this.nThreads;
+        Objects.requireNonNull(matrix);
+        final int size = matrix.length / nThreads + matrix.length % nThreads;
         final List<Worker> workers = new ArrayList<>(this.nThreads);
-        for (int i = 0; i < list.size(); i += size) {
-            workers.add(new Worker(list, i, size));
+        for (int i = 0; i < matrix.length; i += size) {
+            workers.add(new Worker(matrix, i, size));
         }
         for (final var worker: workers) {
             worker.start();
         }
         double sum = 0.0;
         for (final var worker: workers) {
-            try {
+            try{
                 worker.join();
                 sum += worker.getResult();
             } catch (InterruptedException e) {
-                throw new IllegalStateException(e);
+                e.printStackTrace();
             }
         }
         return sum;
     }
-    
 }
