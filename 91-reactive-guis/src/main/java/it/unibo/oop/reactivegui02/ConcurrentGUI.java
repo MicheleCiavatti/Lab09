34928a8fe287547bde54837ce34538941eb80f15
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2022-11-22 15:09:41 +0100

    Added functions on buttons

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,6 +2,7 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -34,6 +35,49 @@ public final class ConcurrentGUI extends JFrame {
         panel.add(this.btStop);
         this.add(panel);
         this.setVisible(true);
+
+        final var agent = new Agent();
+        this.btUp.addActionListener(event -> agent.increaseCounting());
+        this.btDown.addActionListener(event -> agent.decreaseCounting());
+        this.btStop.addActionListener(event -> agent.stopCounting());
+
+        new Thread(agent).start();
+    }
+
+    private class Agent implements Runnable {
+
+        private volatile boolean stop;
+        private volatile boolean isDown; //Initially false: counter goes up by default
+        private int counter;
+
+        public void run() {
+            while (!this.stop) {
+                try {
+                    final var nextNumber = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.currentNumber.setText(nextNumber));
+                    this.counter = this.isDown ?
+                    this.counter - 1:
+                    this.counter + 1;
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } 
+        }
+        public void stopCounting() {
+            this.stop = true;
+            ConcurrentGUI.this.btDown.setEnabled(false);
+            ConcurrentGUI.this.btUp.setEnabled(false);
+        }
+
+        public void decreaseCounting() {
+            this.isDown = true;
+        }
+
+        public void increaseCounting() {
+            this.isDown = false;
+        }
+        
     }
 
 }
