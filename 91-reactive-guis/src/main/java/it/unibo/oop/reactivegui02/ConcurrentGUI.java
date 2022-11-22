7e1492a8584edf803f6d280ab461c96c46b99e3f
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2022-11-22 14:53:42 +0100

    Implemented GUI: missing functions for buttons

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -13,4 +13,27 @@ import javax.swing.SwingUtilities;
  * Second example of reactive GUI.
  */
 public final class ConcurrentGUI extends JFrame {
+    
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel currentNumber = new JLabel();
+    private final JButton btUp = new JButton("Up");
+    private final JButton btDown = new JButton("Down");
+    private final JButton btStop = new JButton("Stop");
+
+    
+    public ConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final var panel = new JPanel();
+        panel.add(this.currentNumber);
+        panel.add(this.btUp);
+        panel.add(this.btDown);
+        panel.add(this.btStop);
+        this.add(panel);
+        this.setVisible(true);
+    }
+
 }
